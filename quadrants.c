086a4ca8e371ee60a8f0594046a07e0c02531a8c
lirajohny <johnylira.dev@gmail.com>
2024-04-18 10:35:09 -0300

    improved quadrants functions, now it has less steps

--- quadrants.c
+++ quadrants.c
@@ -17,74 +17,33 @@ void	quadrants_fill(t_data *img, float width, float height)
 	real_x = 2.0 / (width / 2.0);
 	imaginary_y = 2.0 / (height / 2.0);
 	i = 0;
-	// I quadrant
-	while (i < (height / 2))
-	{
-		j = width / 2;
-		while (j < width)
-		{
-			c.real = (j - (width / 2.0)) * real_x;
-			c.i = (2.0 - (i * imaginary_y));
-			int	value = interations(z, c);
-			put_my_px(img, j, i, value);
-			j++;
-
-		}
-		i++;
-		z.i-=z.i;
-	}
-	// II qudrant
-	i = height / 2;
-	while (i < height)
-	{
-		j = width / 2;
-		while (j < width)
-		{
-			c.real = (j - (width / 2)) * real_x;
-			c.i = ((height / 2) - i) * imaginary_y;
-			int	value = interations(z, c);
-			put_my_px(img, j, i, value);
-			j++;
-		}
-		i++;
-		z.i-=z.i;
-	}
-	// III quadrant
-	i = height / 2;
 	while (i < height)
 	{
 		j = 0;
-		while (j < (width / 2))
+		while (j < width)
 		{
-			c.real = (j - (width / 2)) * real_x;
-			c.i = ((height / 2) - i) * imaginary_y;
-			//c.i = (2 - (i * imaginary_y));
-			int	value = interations(z, c);
-			put_my_px(img, j, i, value);
+			if (i <= 0 && i < height / 2)
+			{
+				// I quadrant && VI quadrant
+				c.real = (j - (width / 2)) * real_x;
+				c.i = (2 - (i * imaginary_y));
+				int	value = interations(z, c);
+				put_my_px(img, j, i, value);
+			}
+			else
+			{
+				// II qudrant && III quadrant
+				c.real = (j - (width / 2)) * real_x;
+				c.i = ((height / 2) - i) * imaginary_y;
+				int	value = interations(z, c);
+				put_my_px(img, j, i, value);
+			}
 			j++;
 		}
-		i++;
 		z.i-=z.i;
-	}
-
-	// IV quadrant
-	i = 0;
-	while (i < (height / 2))
-	{
-		j = 0;
-		while (j < (width  / 2))
-		{
-			c.real = (j - (width / 2)) * real_x;
-			c.i = (2 - (i * imaginary_y));
-			int	value = interations(z, c);
-			put_my_px(img, j, i, value);
-			j++;
-		}
 		i++;
-		z.i-=z.i;
 	}
 }
-
 /* learn how an image is writen/read in c
 void	change_colors(t_data *img, float width, float height)
 {
