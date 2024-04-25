62e0e206cfbcdd3f8da85c4bc69fa23fbb32cea0
jlira <johnyliraone@gmail.com>
2024-04-25 10:40:42 -0300

    cleaning

--- bkp/size.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	for (int i = 1; i <= 640; i++)
-	{
-		printf("\033[0;32m0\033[0m");   // Verde
-	}
-	printf("\033[0;31mthis should be a new line\033[0m\n");
-}
